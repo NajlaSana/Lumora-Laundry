e3e79263b693aed52d2d57d92f5c24044f2391d7
DevaAriantii <devaarianti.id@gmail.com>
2025-03-16 15:58:30 +0700

    Create HomeView.vue
    codingan homeview

---
+++ HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <OutletList />
+  </div>
+</template>
+
+<script>
+import OutletList from "@/components/OutletList.vue";
+
+export default {
+  components: {
+    OutletList
+  }
+};
+</script>
